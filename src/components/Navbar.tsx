eb2aac230a93a6c33532f7432d0b81ab889f4a0c
Allyhere <camilomichelettors@gmail.com>
2025-10-05 14:22:52 -0300

    feat: implementa estilos no navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router";
+
 function Navbar() {
-  return <header>Navbar</header>;
+  return (
+    <header className="flex w-full items-center justify-between bg-indigo-500 px-8 py-4 text-gray-50">
+      <div className="grid gap-0.5">
+        <h1 className="text-lg font-bold">Camilo Micheletto</h1>
+        <p>camili.silva@gmail.com</p>
+      </div>
+      <nav>
+        <ul className="aline-items-center flex gap-x-4 text-base font-bold">
+          <li>
+            <Link to="/" className="hover:underline">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="hover:underline">
+              Registrar
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export { Navbar };
