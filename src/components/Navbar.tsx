650ff06a4b7cb8d98c406e3148f61659bbc04fad
WillahelmGui <Ribeiro.guicosta@gmail.com>
2025-10-05 14:32:34 -0300

    feat: captura usuário logado do localstore

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router";
 
 function Navbar() {
+  const { nomeUsuario, email } = JSON.parse(
+    localStorage.getItem("user") ||
+      JSON.stringify({ nomeUsuario: "Guest", email: "" }),
+  );
+
   return (
     <header className="flex w-full items-center justify-between bg-indigo-500 px-8 py-4 text-gray-50">
       <div className="grid gap-0.5">
-        <h1 className="text-lg font-bold">Camilo Micheletto</h1>
-        <p>camili.silva@gmail.com</p>
+        <h1 className="text-lg font-bold">{nomeUsuario}</h1>
+        <p>{email}</p>
       </div>
       <nav>
         <ul className="aline-items-center flex gap-x-4 text-base font-bold">
