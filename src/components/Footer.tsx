43280daef85d0a1c9286bbd42677593c8ca64cd1
WillahelmGui <Ribeiro.guicosta@gmail.com>
2025-10-05 14:38:33 -0300

    feat: adiciona estilos do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="mt-auto w-full border-t bg-indigo-500 p-4 text-center text-sm text-gray-50">
       &copy; {new Date().getFullYear()} Camilo, Carlos e Guilherme
     </footer>
   );
